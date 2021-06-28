abc40993efaf2ed6c11e36792251a12d2134e44a
João Almeida <joao.pedrosa@luminiitsolutions.com>
2021-06-28 13:56:30 -0300

    project hr-config-server created

---
+++ hrconfig-server/src/main/java/com/devsuperior/hraconfigserver/HrconfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hraconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrconfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrconfigServerApplication.class, args);
+	}
+
+}
---
+++ hrconfig-server/src/test/java/com/devsuperior/hraconfigserver/HrconfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hraconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
