ff736ad0296d594c896ab2c21f7523604b95efc9
João Almeida <joao.pedrosa@luminiitsolutions.com>
2021-07-04 17:00:55 -0300

    git private repository

--- hrconfig-server/src/main/java/com/devsuperior/hraconfigserver/HrconfigServerApplication.java
+++ hrconfig-server/src/main/java/com/devsuperior/hraconfigserver/HrconfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.devsuperior.hraconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,8 +10,22 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class HrconfigServerApplication {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+	
+	@Value("${spring.cloud.config.server.git.password}")
+	private String pass;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrconfigServerApplication.class, args);
 	}
+
+	//para exibir as variáveis de ambiente
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.print("username= " + username );
+//		System.out.println("pass= " + pass);
+//		
+//	}
 
 }
