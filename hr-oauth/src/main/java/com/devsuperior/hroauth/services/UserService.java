ada3f710023e0dd388595a995fd5cb520d48c7b8
João Almeida <joao.pedrosa@luminiitsolutions.com>
2021-07-06 10:18:08 -0300

    zuul authorization

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,47 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	private static Logger Logger= LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			Logger.error("email not found: " + email);
+			throw new IllegalArgumentException("email not found");
+			
+		} 
+		Logger.info("found email:" + email);
+		return user;
+				
+		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			Logger.error("email not found: " + username);
+			throw new UsernameNotFoundException("email not found");
+			
+		} 
+		Logger.info("found email:" + username);
+		return user;
+	}
+
+}
