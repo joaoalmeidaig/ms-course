962202ecc9ee59229b9eac71accbafb4452d6f41
João Almeida <joao.pedrosa@luminiitsolutions.com>
2021-07-06 11:14:10 -0300

    security configs to confg server

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -18,7 +18,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	private static final String[] PUBLIC= {"/hr-oauth/oauth/token"};
 	private static final String[] OPERATOR = {"/hr-worker/**"};
-	private static final String[] ADMIN= {"/hr-payroll/**", "hr-user/**"};
+	private static final String[] ADMIN= {"/hr-payroll/**", "hr-user/**", "/actutatuor/**","/hr-worker/actutatuor/**","hr-oauth/actutatuor/**"};
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
